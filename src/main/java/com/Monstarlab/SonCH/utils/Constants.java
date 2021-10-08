77848b75bedca4f4d470126e720225a0e6aa7f1c
Son Hoang <88713924+Sonch2k@users.noreply.github.com>
2021-10-08 17:16:17 +0700

    Update T0004(integrated Swagger UI)

--- src/main/java/com/Monstarlab/SonCH/utils/Constants.java
+++ src/main/java/com/Monstarlab/SonCH/utils/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String BEARER_HEADER = "Bearer ";
 
     public static final String HEADER_NAME_JWT = "Authorization";
+    public static final String HEADER = "header";
+    public static final String JWT = "JWT";
 }
